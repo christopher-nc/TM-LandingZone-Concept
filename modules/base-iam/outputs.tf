63d8001e6618de2789bfcbde3c8b347f857c8957
Christopher Dearie <christopher.dearie@nclouds.com>
2025-06-02 12:33:06 -0700

    Action Bronson

--- modules/base-iam/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "deployed_roles" {
-  description = "List of accounts where IAM roles were deployed"
-  value       = [for account in data.aws_organizations_organization.org.accounts : account.id]
-}
