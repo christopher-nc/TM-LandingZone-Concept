f132ea9645870b6024e83ec3ad1038e6b0a7d574
Christopher Dearie <christopher.dearie@nclouds.com>
2025-06-03 11:28:17 -0700

    LZA POC

--- stacks/global/main.tf
+++ stacks/global/main.tf
@@ -7,3 +7,24 @@ resource "aws_organizations_organization" "main" {
   ]
   feature_set = "ALL"
 }
+
+
+resource "aws_cloudformation_stack" "lza" {
+  name = "AWSAccelerator-InstallerStack"
+  
+  template_url = "https://lza-aws-accelerator-installer-bucket.s3.us-east-1.amazonaws.com/AWSAccelerator-InstallerStack.template.json"
+
+  parameters = {
+    RepositoryBranchName              = "main"
+    ManagementAccountEmail            = "lza@lza.com"
+    LogArchiveAccountEmail            = "lza@lza.com"
+    AuditAccountEmail                 = "lza@lza.com"
+    EnableApprovalStage               = "true"
+    ApprovalStageNotifyEmailList      = "lza@lza.com"
+    ControlTowerEnabled               = "true"
+  }
+  capabilities = ["CAPABILITY_IAM"]
+
+  disable_rollback = true
+  
+}
---
+++ stacks/global/stack.tm.hcl
@@ -0,0 +1,11 @@
+stack {
+  id          = "7b5f4d89-70a7-42f0-972f-3be8550e65df"
+  name        = "Management"
+  description = "The Root Account for the AWS Organization"
+  tags = [
+    "aws",
+    "billing",
+    "management",
+    "production",
+  ]
+}
