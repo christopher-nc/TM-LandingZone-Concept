2ede74042d01b52642c48fd1c74cf42ac0a9e535
Christopher Dearie <christopher.dearie@nclouds.com>
2025-06-05 10:28:46 -0700

    Remove services

--- stacks/organization/main.tf
+++ stacks/organization/main.tf
@@ -1,8 +1,6 @@
 resource "aws_organizations_organization" "main" {
   aws_service_access_principals = [
-    "cloudtrail.amazonaws.com",
-    "config.amazonaws.com",
-    "sso.amazonaws.com"
+
   ]
   feature_set = "ALL"
 }
